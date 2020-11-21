b03c3d55119c1f19ef5627e110ca7a67fdd136b6
leminhmy <50837444+leminhmy@users.noreply.github.com>
2020-11-21 10:44:21 +0700

    update rating detail product

--- LeMinhMy/app/src/main/java/com/example/leminhmy/ProductSpecificationModel.java
+++ LeMinhMy/app/src/main/java/com/example/leminhmy/ProductSpecificationModel.java
@@ -2,10 +2,42 @@ package com.example.leminhmy;
 
 public class ProductSpecificationModel {
 
+    public static final int SPECIFICATION_TITLE = 0;
+    public static final int SPECIFICATION_BODY = 1;
+
+    private int type;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    //////specifiation title
+    private String title;
+
+    public ProductSpecificationModel(int type, String title) {
+        this.type = type;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    //////specifiation title
+
+    //////specifiation body
     private String featureName;
     private String featureValue;
 
-    public ProductSpecificationModel(String featureName, String featureValue) {
+    public ProductSpecificationModel(int type, String featureName, String featureValue) {
+        this.type = type;
         this.featureName = featureName;
         this.featureValue = featureValue;
     }
@@ -25,4 +57,8 @@ public class ProductSpecificationModel {
     public void setFeatureValue(String featureValue) {
         this.featureValue = featureValue;
     }
+
+    //////specifiation body
+
+
 }
